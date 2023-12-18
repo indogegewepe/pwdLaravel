fa6bc265ff77c260cdaf8f95e3cb977aebeeac41
Sedenter <64572332+indogegewepe@users.noreply.github.com>
2023-12-19 04:22:17 +0700

    anjay
    undefined

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Test Laravel BAGAS</title>
+    <title>Test Laravel</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 </head>
-<body style="background: lightgray">
+<body style="background: darkgray">
 
     <div class="container mt-5">
         <div class="row">
